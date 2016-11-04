78d438e2ff6e37876e4f2c50833f4de19da8147e
Robin Choi <robinchoii@gmail.com>
2016-11-03 17:16:26 -0700

    submit a question functional

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 #discuss.io assignment
+Features summary:
+- adding/removing/editing of screening questions
+- defining the question (textarea)
+- defining/deleting answer options
+- saving (locally) a current state of the question
+- some checkboxes which were mentioned in the video clip
+- dragging/dropping of questions (mentioned in the clip)
--- assignment.js
+++ assignment.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
   console.log('im ready!')
+  $('form').hide()
+  $('#cancelButton').hide()
 
   var listOfQuestions = [
     {question: "Question 1", answers: [], options: [], shuffle: false, noneOfTheAbove: false},
@@ -17,8 +19,60 @@ $(document).ready(function() {
     tr.append("<td>" + listOfQuestions[i-1].question+ "</td>")
     tr.append("<td>" + "<button class='btn btn-info'>Edit</button>"+ "</td>")
     tr.append("<td>" + "<button class='btn btn-danger'>Delete</button>"+ "</td>")
-
   };
+
+  $("#newQuestionButton").on('click', function() {
+    $('form').slideDown(500);
+    $('#cancelButton').fadeIn(500);
+  });
+  $("#cancelButton").on("click", function() {
+    $('#questionForm')[0].reset();
+    $('#cancelButton').hide()
+    $('form').slideUp(500);
+  });
+  $("#addAnswerButton").on("click", function(e) {
+    e.preventDefault();
+    console.log("add a new answer");
+  });
+  $("#submitQuestionButton").on("click", function() {
+    console.log('submit!');
+    newQuestion();
+    $('#cancelButton').hide()
+  });
+
+  function newQuestion() {
+    var question = {};
+    question.question = document.questionForm.question.value;
+    question.answers = {
+      'must-select': 'answer1',
+
+      'may-select': 'answer2',
+      'teriminate-if-select': 'answer3'
+    };
+    if ($("input[name='noneOfTheAbove']").is(':checked')) {
+      question.noneOfTheAbove = true;
+    } else {
+      question.noneOfTheAbove = false;
+    };
+    if ($("input[name='shuffle']").is(':checked')) {
+      question.shuffle = true;
+    } else {
+      question.shuffle = false;
+    };
+
+    // console.log(question)
+
+    var tbody = $("tbody")
+    var tr = $("<tr></tr>").appendTo(tbody);
+    tr.append("<td>" + listOfQuestions.length + "</td>");
+    tr.append("<td>" + question.question+ "</td>")
+    tr.append("<td>" + "<button class='btn btn-info'>Edit</button>"+ "</td>")
+    tr.append("<td>" + "<button class='btn btn-danger'>Delete</button>"+ "</td>")
+
+    $('form').slideUp(500)
+    $('#questionForm')[0].reset()
+  };
+
 
 
 
